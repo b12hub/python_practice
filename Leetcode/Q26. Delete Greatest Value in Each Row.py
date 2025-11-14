e6909366e63d0e7cf888cbec37d1c91245f62126
Bobur <ahmedovzulfiya56@gmail.com>
2025-11-14 17:38:44 +0500

    Delete Leetcode directory

--- Leetcode/Q26. Delete Greatest Value in Each Row.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-class Solution:
-    def deleteGreatestValue(self ,grid: List[List[int]]):
-
-        return [item for item in  range(len(grid)-1,-1 ,-1 )]
-obj = Solution()
-print(obj.deleteGreatestValue([[1, 2, 4], [3, 3, 1]]))
